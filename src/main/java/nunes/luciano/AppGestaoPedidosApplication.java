b0eaeda6aa6058196f73ba146274fb3c915da191
Luciano Nunes Fonseca <lucianonunesfonseca@gmail.com>
2020-08-17 18:19:40 -0300

    Projeto criado

---
+++ src/main/java/nunes/luciano/AppGestaoPedidosApplication.java
@@ -0,0 +1,13 @@
+package nunes.luciano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestaoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestaoPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nunes/luciano/AppGestaoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package nunes.luciano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestaoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
