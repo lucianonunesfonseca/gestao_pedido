9b2b09e05180ee8991d3838f09090bf216df827b
Luciano Nunes Fonseca <lucianonunesfonseca@gmail.com>
2020-08-17 21:03:51 -0300

    Listando todas categorias com DTO

--- src/main/java/nunes/luciano/services/CategoriaService.java
+++ src/main/java/nunes/luciano/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package nunes.luciano.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import nunes.luciano.domain.Categoria;
 import nunes.luciano.repositories.CategoriaRepository;
+import nunes.luciano.services.exceptions.DataIntegrityException;
 import nunes.luciano.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,11 +22,29 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		
+
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
