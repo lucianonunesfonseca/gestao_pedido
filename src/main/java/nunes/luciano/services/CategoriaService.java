c5d429def86ffe7a5231955ecad3a6d55b791563
Luciano Nunes Fonseca <lucianonunesfonseca@gmail.com>
2020-08-17 21:27:58 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/nunes/luciano/services/CategoriaService.java
+++ src/main/java/nunes/luciano/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import nunes.luciano.domain.Categoria;
+import nunes.luciano.dto.CategoriaDTO;
 import nunes.luciano.repositories.CategoriaRepository;
 import nunes.luciano.services.exceptions.DataIntegrityException;
 import nunes.luciano.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
