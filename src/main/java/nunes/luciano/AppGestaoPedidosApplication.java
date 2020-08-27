631c9982b4a0e5cf71c64a6b9fdee78ca3664f7b
Luciano Nunes Fonseca <lucianonunesfonseca@gmail.com>
2020-08-27 16:48:01 -0300

    Enviando imagem via endpoint

--- src/main/java/nunes/luciano/AppGestaoPedidosApplication.java
+++ src/main/java/nunes/luciano/AppGestaoPedidosApplication.java
@@ -1,17 +1,13 @@
 package nunes.luciano;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import nunes.luciano.services.S3Service;
-
 @SpringBootApplication
 public class AppGestaoPedidosApplication implements CommandLineRunner {
 	
-	@Autowired
-	private S3Service s3Service;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppGestaoPedidosApplication.class, args);
@@ -19,7 +15,7 @@ public class AppGestaoPedidosApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("D:\\Arquivos importantes\\projeto-help-services\\imagens\\iphone6.png");
+		
 		
 	}
 }
--- src/main/java/nunes/luciano/services/ClienteService.java
+++ src/main/java/nunes/luciano/services/ClienteService.java
@@ -1,5 +1,6 @@
 package nunes.luciano.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import nunes.luciano.domain.Cidade;
 import nunes.luciano.domain.Cliente;
@@ -35,6 +37,8 @@ public class ClienteService {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		
@@ -104,6 +108,11 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		
+		return s3Service.uploadFile(multipartFile);
 	}
 	
 	
